d35ce5802b00d156536dbbf2ef81647c73025d6d
aaivey22 <angelawings22@gmail.com>
2020-04-28 23:15:17 -0400

    debugging the post method in apiroutesjs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,4 @@
 
-
 const fs = require("fs"); // Require is a method
 
 // Linking api routes to a series of "data" sources that hold arrays of information on the new/old note entries.
@@ -16,40 +15,35 @@ module.exports = function (app) {
     })
 
     // API POST request uses the route to save new user entries by pushing them into the postEntries arr.
-    // A 
     app.post("/api/notes", (req, res) => {
         let newEntry = req.body;
         newEntry.id = Date.now().toString();
         let postEntries = [];
-
         fs.readFile("db/db.json", "utf8", (err, data) => { // The readFile is anonymous and contains three values.
             if (err) throw err
             else {
-                postEntries = JSON.parse(data);
+                postEntries = JSON.stringify(data);
             }
         })
+
         postEntries.push(newEntry);
         fs.writeFile("db/db.json", JSON.stringify(postEntries), (err) => {
             if (err) throw err
             else {
                 console.log("entry posted")
             }
-
         })
-
         console.log(req.body)
         res.json(newEntry);
     });
+
+    // app.delete("/api/notes/:id", (req, res) => {
+    //     let id = req.params.id;
+    //     // let deleteEntries = [];
+    //     fs.readFile("db/db.json", "utf8", (err, data) => {
+    //         const result = postEntries.filter(id => id === id[i]);
+    //         console.log(result);
+
+    //     })
+    // }); 
 }
-
-
-
-
-
-
-
-
-
-
-
-// Date.now().toString();
