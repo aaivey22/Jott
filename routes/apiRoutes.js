cde1076e3fd456e179d75f4474746446204c7e3d
aaivey22 <angelawings22@gmail.com>
2020-04-30 21:34:13 -0400

    made changes to apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,13 @@
 
 const fs = require("fs"); // Require is a method
-
+let postEntries = [];
 // Linking api routes to a series of "data" sources that hold arrays of information on the new/old note entries.
 // API GET request uses the route to retrieve array of note data to display.
 module.exports = function (app) {
     app.get("/api/notes", (req, res) => {
         fs.readFile("db/db.json", "utf8", (err, data) => { // The readFile is anonymous and contains three values.
             if (err) throw err
-            else {
-                console.log(JSON.parse(data))
-                res.json(JSON.parse(data))
-            }
+            res.json(JSON.parse(data))
         })
     })
 
@@ -18,13 +15,14 @@ module.exports = function (app) {
     app.post("/api/notes", (req, res) => {
         let newEntry = req.body;
         newEntry.id = Date.now().toString();
-        let postEntries = [];
-        fs.readFile("db/db.json", "utf8", (err, data) => { // The readFile is anonymous and contains three values.
-            if (err) throw err
-            else {
-                postEntries = JSON.stringify(data);
-            }
-        })
+        // fs.writeFile("db/db.json", "", (err) => { // The readFile is anonymous and contains three values.
+        //     if (err) throw err
+        //     // else {
+        //     //     // data.push(newEntry);
+        //     //     // console.log(data);
+        //     //     postEntries = JSON.stringify(data);
+        //     // }
+        // })
 
         postEntries.push(newEntry);
         fs.writeFile("db/db.json", JSON.stringify(postEntries), (err) => {
@@ -36,14 +34,28 @@ module.exports = function (app) {
         console.log(req.body)
         res.json(newEntry);
     });
+// delete method is defining 3 variables
+    app.delete("/api/notes/:id", (req, res) => {
+        let id = req.params.id;
+        let deletedEntry = postEntries.find(entry => entry.id === id)
+        let indexEntry = postEntries.indexOf(deletedEntry) //gets the array index value of note with correct id
+        postEntries.splice(indexEntry, 1)
 
-    // app.delete("/api/notes/:id", (req, res) => {
-    //     let id = req.params.id;
-    //     // let deleteEntries = [];
-    //     fs.readFile("db/db.json", "utf8", (err, data) => {
-    //         const result = postEntries.filter(id => id === id[i]);
-    //         console.log(result);
+        fs.writeFile("db/db.json", JSON.stringify(postEntries), (err) => {
+            if (err) throw err
+        })
 
-    //     })
-    // }); 
+        // fs.readFile("db/db.json", "utf8", (err, data) => {
+        //     // const result = postEntries.filter(id => id === id[i]);
+        //     // console.log(result);
+        //     let deleteEntries = JSON.parse(data)
+        //     let deletedEntry = deleteEntries.find(entry => entry.id === id)
+        //     let indexEntry = deleteEntries.indexOf(deletedEntry)
+        //     deleteEntries.splice(indexEntry, 1)
+        //     console.log(deleteEntries);
+        //     console.log(indexEntry);
+        //     console.log(deletedEntry);
+        // })
+
+    });
 }
